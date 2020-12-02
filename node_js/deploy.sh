115d2e7b13e39e82de7188e3ee0188bfc0f8c109
KIM JUNG BIN <kjbin0420@naver.com>
2020-12-02 17:48:47 +0900

    Update deploy.sh

--- node_js/deploy.sh
+++ node_js/deploy.sh
@@ -8,4 +8,4 @@ git pull
 docker build -t node_express .
 
 # Deploy Docker Container
-docker run -p ${node_express_port}:${node_express_port} --network ${db_network_name} -d
+docker run -p 3000:3000 --network db -d
